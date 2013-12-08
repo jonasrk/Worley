f62678d90af13e8c0e9b2c2cd8e386c03aeba1c6
Jonas <jonas@jonass-mbp.fritz.box>
2013-12-08 12:30:38 +0100

    initial commit

---
+++ Worley/Worley/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Worley
+//
+//  Created by Jonas on 08/12/13.
+//  Copyright (c) 2013 Jonas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
